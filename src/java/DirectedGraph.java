a8daede88ee09fe052ff84062cfbc3b7bcceef13
yrmint <zhilkinal@yandex.ru>
2022-03-03 22:55:50 +0300

    task

--- src/java/DirectedGraph.java
+++ src/java/DirectedGraph.java
@@ -1,99 +1,167 @@
 import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 public class DirectedGraph {
+    //вершина
     public static class Vertex {
-        String name;
+        private String name;
 
-        Vertex (String name) {
+        public Vertex (String name) {
             this.name = name;
         }
+
+        @Override
+        public int hashCode() {
+            return name.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof Vertex)) return false;
+            return Objects.equals(this.name, ((Vertex) obj).name);
+        }
     }
 
-
+    //дуга
     public static class Edge {
-        Vertex begin;
-        Vertex end;
-        int value;
+        private final Vertex begin;
+        private final Vertex end;
+        private int value;
 
-        Edge (Vertex begin, Vertex end, int value) {
+        public Edge (Vertex begin, Vertex end, int value) {
             this.begin = begin;
             this.end = end;
+            if (value <= 0) throw new IllegalArgumentException();
             this.value = value;
+
         }
+
+        public Edge (String begin, String end, int value) {
+            this.begin = new Vertex(begin);
+            this.end = new Vertex(end);
+            if (value <= 0) throw new IllegalArgumentException();
+            this.value = value;
+
+        }
+
+        @Override
+        public int hashCode() {
+            return begin.hashCode() + end.hashCode() + value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof Edge)) return false;
+            Edge arc = (Edge) obj;
+            return value == arc.value && begin.equals(arc.begin) && end.equals(arc.end);
+        }
+
     }
 
-    private HashSet <Vertex> verticies = new HashSet<Vertex>();
-    private HashSet <Edge> edges = new HashSet<Edge>();
+    public DirectedGraph () {}
 
-    public Vertex getVertex(String name) {
-        for (Vertex v : verticies) {
-            if (name == v.name) return v;
+    public DirectedGraph (Set<Vertex> vertexes, Set<Edge> edges) {
+        this.vertexes.addAll(vertexes);
+        for (Edge edge: edges) this.addEdge(edge.begin.name, edge.end.name, edge.value);
+    }
+
+    private final HashSet <Vertex> vertexes = new HashSet<Vertex>();
+    private final HashSet <Edge> edges = new HashSet<Edge>();
+
+    //получение вершины по имени
+    private Vertex getVertex(String name) {
+        for (Vertex v : vertexes) {
+            if (name.equals(v.name)) return v;
         }
         return null;
     }
 
-    public Edge getEdge(String begin, String end) {
+    //получение дуги по именам вершин
+    private Edge getEdge(String begin, String end) {
         for (Edge e : edges) {
-            if (e.begin.name == begin & e.end.name == end) return e;
+            if (e.begin.name.equals(begin) & e.end.name.equals(end)) return e;
         }
         return null;
     }
 
+    //добавление вершины
     public void addVertex(String name) {
-        if (this.getVertex(name) == null) {
-            verticies.add((new Vertex(name)));
-            System.out.println("Вершина " + name + " создана");
-        }
-        else System.out.println("Вершина с именем " + name + " уже существует");
+        vertexes.add((new Vertex(name)));
     }
 
+    //удаление вершины
     public void deleteVertex(String name) {
-        if (this.getVertex(name) != null) {
-            verticies.remove(this.getVertex(name));
-            System.out.println("Вершина " + name + " удалена");
+        try {
+            edges.removeAll(getIngoingEdges(name));
+            edges.removeAll(getOutgoingEdges(name));
+            vertexes.remove(this.getVertex(name));
         }
-        else System.out.println("Вершины с именем " + name + " не существует");
+        catch (NullPointerException e) {}
     }
 
+    //изменение имени вершины
     public void changeVertexName(String oldName, String newName) {
-        if (this.getVertex(oldName) != null) {
+        try {
             this.getVertex(oldName).name = newName;
-            System.out.println("Вершина " + oldName + " переименована в " + newName);
         }
-        else System.out.println("Вершины с именем " + oldName + " не существует");
+        catch (NullPointerException e) {}
     }
 
+    //добавление дуги
     public void addEdge(String begin, String end, int value) {
-        if (this.getEdge(begin, end) == null) {
-            if (this.getVertex(begin) == null) this.addVertex(begin);
-            if (this.getVertex(end) == null) this.addVertex(end);
+        if (!begin.equals(end) && this.getEdge(begin, end) == null){
+            this.addVertex(begin);
+            this.addVertex(end);
             edges.add(new Edge(getVertex(begin), getVertex(end), value));
-            System.out.println("Ребро из " + begin + " в " + end + " весом " + value + " создано");
         }
-        else System.out.println("Ребро из " + begin + " в " + end + " уже существует");
     }
 
-    public void getVerticies() {
-        for (Vertex v : verticies) System.out.print(v.name + " ");
-        System.out.println();
+    //удаление дуги
+    public void deleteEdge(String begin, String end) {
+        edges.remove(getEdge(begin, end));
     }
 
-    public void getEdges() {
-        for (Edge e : edges) System.out.println("Из " + e.begin.name + " в " + e.end.name + ", вес " + e.value);
+    //изменение веса дуги
+    public void changeEdgeValue (String begin, String end, int newValue) {
+        try {
+            if (newValue > 0) getEdge(begin, end).value = newValue;
+        }
+        catch (NullPointerException e) {}
     }
 
-    public static void main (String[] args) {
-        DirectedGraph myGraph = new DirectedGraph();
-        myGraph.addVertex("A");
-        myGraph.addVertex("A");
-        myGraph.addVertex("B");
-        myGraph.getVerticies();
-        myGraph.addEdge("L", "B", 2);
-        myGraph.addEdge("L", "B", 2);
-        myGraph.addEdge("B", "L", 2);
-        myGraph.getEdges();
-        System.out.println();
-        myGraph.changeVertexName("L", "I");
-        myGraph.getEdges();
+    //получение списка исходящих из вершины дуг по имени вершины
+    public HashSet<Edge> getOutgoingEdges (String name) {
+        HashSet<Edge> result = new HashSet<>();
+        for (Edge edge: edges) {
+            if (edge.begin.equals(getVertex(name))) {
+                result.add(edge);
+            }
+        }
+        return result;
+    }
+
+    //получение списка входящих в вершину дуг по имени вершины
+    public HashSet<Edge> getIngoingEdges (String name) {
+        HashSet<Edge> result = new HashSet<>();
+        for (Edge edge: edges) {
+            if (edge.end.equals(getVertex(name))) {
+                result.add(edge);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        return vertexes.hashCode() + edges.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (!(obj instanceof DirectedGraph)) return false;
+        DirectedGraph graph = (DirectedGraph) obj;
+        return edges.equals(graph.edges) && vertexes.equals(graph.vertexes);
     }
 }
--- src/test/test.java
+++ src/test/test.java
@@ -1,8 +1,165 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import java.util.Set;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class test {
-    @Test
-    public void testAdd() {
 
+    @Test
+    public void equalsVertexes() {
+        DirectedGraph.Vertex a = new DirectedGraph.Vertex("A");
+        DirectedGraph.Vertex a1 = new DirectedGraph.Vertex("A");
+        DirectedGraph.Vertex a2 = new DirectedGraph.Vertex("a");
+        DirectedGraph.Vertex b = new DirectedGraph.Vertex("B");
+
+        assertTrue(a.equals(a1));
+        assertFalse(a.equals(b));
+        assertFalse(a.equals(a2));
     }
+
+    @Test
+    public void equalsEdges() {
+        DirectedGraph.Edge ab = new DirectedGraph.Edge(new DirectedGraph.Vertex("A"), new DirectedGraph.Vertex("B"), 5);
+        DirectedGraph.Edge ab1 = new DirectedGraph.Edge(new DirectedGraph.Vertex("A"), new DirectedGraph.Vertex("B"), 5);
+        DirectedGraph.Edge ac = new DirectedGraph.Edge(new DirectedGraph.Vertex("A"), new DirectedGraph.Vertex("C"), 6);
+
+        assertTrue(ab.equals(ab1));
+        assertFalse(ab.equals(ac));
+    }
+
+    @Test
+    public void equalsGraphs() {
+        DirectedGraph graph1 = new DirectedGraph(Set.of(), Set.of(new DirectedGraph.Edge("A", "B", 1),
+                new DirectedGraph.Edge("C", "B", 2),
+                new DirectedGraph.Edge("A", "C", 3)));
+
+        DirectedGraph graph2 = new DirectedGraph(Set.of(), Set.of(new DirectedGraph.Edge("A", "B", 1),
+                new DirectedGraph.Edge("C", "B", 2),
+                new DirectedGraph.Edge("A", "C", 3)));
+
+        DirectedGraph graph3 = new DirectedGraph(Set.of(), Set.of(new DirectedGraph.Edge("A", "B", 1),
+                new DirectedGraph.Edge("C", "B", 2),
+                new DirectedGraph.Edge("A", "F", 3)));
+
+        DirectedGraph graph4 = new DirectedGraph(Set.of(), Set.of(new DirectedGraph.Edge("A", "B", 1),
+                new DirectedGraph.Edge("C", "B", 2),
+                new DirectedGraph.Edge("A", "C", 1000)));
+
+        assertTrue(graph1.equals(graph2));
+        assertTrue(graph1.equals(graph1));
+        assertFalse(graph1.equals(graph3));
+        assertFalse(graph1.equals(graph4));
+    }
+
+    @Test
+    public void addVertex() {
+        DirectedGraph myGraph = new DirectedGraph();
+        myGraph.addVertex("A");
+        myGraph.addVertex("A");
+        myGraph.addVertex("C");
+        myGraph.addVertex("B");
+
+        assertEquals(new DirectedGraph(Set.of(new DirectedGraph.Vertex("A"),
+                new DirectedGraph.Vertex("C"),
+                new DirectedGraph.Vertex("B")), Set.of()), myGraph);
+    }
+
+    @Test
+    public void deleteVertex() {
+        DirectedGraph myGraph = new DirectedGraph(Set.of(new DirectedGraph.Vertex("A"),
+                new DirectedGraph.Vertex("B"),
+                new DirectedGraph.Vertex("C")),
+                Set.of(new DirectedGraph.Edge("A", "B", 5),
+                        new DirectedGraph.Edge("C", "A", 3)));
+        myGraph.deleteVertex("A");
+
+        assertEquals(new DirectedGraph(Set.of(new DirectedGraph.Vertex("B"),
+                new DirectedGraph.Vertex("C")), Set.of()), myGraph);
+    }
+
+    @Test
+    public void changeVertexName() {
+        DirectedGraph myGraph = new DirectedGraph(Set.of(new DirectedGraph.Vertex("A"),
+                new DirectedGraph.Vertex("B"),
+                new DirectedGraph.Vertex("C")),
+                Set.of(new DirectedGraph.Edge("A", "B", 5),
+                        new DirectedGraph.Edge("C", "A", 3)));
+        myGraph.changeVertexName("A", "F");
+
+        DirectedGraph myGraph2 = myGraph;
+        myGraph2.changeVertexName("P", "F");
+
+        assertEquals(new DirectedGraph(Set.of(new DirectedGraph.Vertex("F"),
+                new DirectedGraph.Vertex("B"),
+                new DirectedGraph.Vertex("C")),
+                Set.of(new DirectedGraph.Edge("F", "B", 5),
+                        new DirectedGraph.Edge("C", "F", 3))), myGraph);
+        assertEquals(myGraph, myGraph2);
+    }
+
+    @Test
+    public void addEdge() {
+        DirectedGraph myGraph = new DirectedGraph();
+        myGraph.addEdge("A", "B", 1);
+        myGraph.addEdge("A", "C", 2);
+        myGraph.addEdge("B", "C", 3);
+        myGraph.addEdge("B", "C", 1000);
+
+        assertEquals(new DirectedGraph(Set.of(), Set.of(
+                new DirectedGraph.Edge("A", "B", 1),
+                new DirectedGraph.Edge("A", "C", 2),
+                new DirectedGraph.Edge("B", "C", 3)
+        )), myGraph);
+    }
+
+    @Test
+    public void deleteEdge() {
+        DirectedGraph myGraph = new DirectedGraph(Set.of(), Set.of(new DirectedGraph.Edge("A", "B", 5),
+                new DirectedGraph.Edge("B", "C", 1),
+                new DirectedGraph.Edge("A", "C", 2)));
+        myGraph.deleteEdge("A", "B");
+
+        assertEquals(new DirectedGraph(Set.of(), Set.of(new DirectedGraph.Edge("B", "C", 1),
+                new DirectedGraph.Edge("A", "C", 2))), myGraph);
+    }
+
+    @Test
+    public void changeEdgeValue() {
+        DirectedGraph myGraph = new DirectedGraph(Set.of(), Set.of(new DirectedGraph.Edge("A", "B", 5),
+                new DirectedGraph.Edge("B", "C", 1),
+                new DirectedGraph.Edge("A", "C", 2)));
+        myGraph.changeEdgeValue("A", "B", 100);
+
+        DirectedGraph myGraph2 = myGraph;
+        myGraph2.changeEdgeValue("P", "H", 42);
+
+        assertEquals(new DirectedGraph(Set.of(), Set.of(new DirectedGraph.Edge("A", "B", 100),
+                new DirectedGraph.Edge("B", "C", 1),
+                new DirectedGraph.Edge("A", "C", 2))), myGraph);
+        assertEquals(myGraph, myGraph2);
+    }
+
+    @Test
+    public void getOutgoingEdges() {
+        DirectedGraph myGraph = new DirectedGraph(Set.of(), Set.of(new DirectedGraph.Edge("A", "B", 5),
+                new DirectedGraph.Edge("B", "C", 1),
+                new DirectedGraph.Edge("A", "C", 2)));
+
+        assertEquals(Set.of(new DirectedGraph.Edge("A", "B", 5),
+                new DirectedGraph.Edge("A", "C", 2)), myGraph.getOutgoingEdges("A"));
+
+        assertEquals(Set.of(), myGraph.getOutgoingEdges("C"));
+    }
+
+    @Test
+    public void getIngoingEdges() {
+        DirectedGraph myGraph = new DirectedGraph(Set.of(), Set.of(new DirectedGraph.Edge("A", "B", 5),
+                new DirectedGraph.Edge("B", "C", 1),
+                new DirectedGraph.Edge("A", "C", 2)));
+
+        assertEquals(Set.of(new DirectedGraph.Edge("B", "C", 1),
+                new DirectedGraph.Edge("A", "C", 2)), myGraph.getIngoingEdges("C"));
+
+        assertEquals(Set.of(), myGraph.getIngoingEdges("A"));
+    }
+
 }
